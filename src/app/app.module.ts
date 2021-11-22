e1ddb607c4b94b866ac3930c221220b65b089639
manuel dellagala <manuel.dellagala@gmail.com>
2021-11-22 08:49:15 +0100

    comunicazione tra component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/table/table.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.scss']
+})
+export class TableComponent implements OnInit {
+
+  @Input() users=[{nome:'',citta:''}];
+
+  constructor() { }
+
+  ngOnInit(){
+  }
+
+}
